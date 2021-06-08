7b2f076d26baeda194faa7b50c44c7065d334f30
Samuel Myres <samiam2013@gmail.com>
2021-06-07 19:21:53 -0500

    hello !

---
+++ ex_02_name_greeting.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Write a program that asks the user for their name 
+#   and greets them with their name.
+
+read -p "What's your name user?: " name;
+
+echo "Hi! Nice to meet you, $name";
+
+exit;
