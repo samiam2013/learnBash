e15667bb8b724aa68f6a09631dff42389cf9afe8
Samuel Myres <samiam2013@gmail.com>
2021-06-07 20:15:16 -0500

    shameless commit

---
+++ ex_08_primes.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Write a program that prints all prime numbers. 
+#   (Note: if your programming language does not 
+#   support arbitrary size numbers, printing all 
+#   primes up to the largest number you can easily 
+#   represent is fine too.)
+
+exit;
