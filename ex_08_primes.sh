118d6da2cbb7e349c783ff4890076e368a86bd85
Samuel Myres <samiam2013@gmail.com>
2021-06-07 20:44:22 -0500

    fuck bash, fuck arbitary precision, fuck stupid bc, fuck it all

--- ex_08_primes.sh
+++ ex_08_primes.sh
@@ -5,4 +5,27 @@
 #   primes up to the largest number you can easily 
 #   represent is fine too.)
 
+candidate=5
+while true; do 
+    let i=2
+    let ceil=`echo "sqrt($candidate)+1" | bc`
+    let is_prime=1
+    printf 'factor ceiling %d\n' $ceil
+    while (() `echo "$i<$ceil" | bc` == 1 )); do
+        echo "$candidate/$i"
+        if (( `echo "$candidate%$i==0" | bc` == 1 )); then
+            let is_prime=0
+            echo "not prime"
+            break
+        fi
+        let i=i+1
+    done
+    if (( $is_prime == 0 )); then 
+        break
+    fi
+    let candidate=candidate+1
+    exit
+done
+
+
 exit;
