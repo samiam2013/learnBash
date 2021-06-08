d5c566d47d46a2a1f4dfd9f584b7b133b4c77251
Samuel Myres <samiam2013@gmail.com>
2021-06-07 19:23:37 -0500

    triangle with a fizzbuzz?

---
+++ ex_05_triangle_fizzbuzz.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Modify the previous program such that only
+#   multiples of three or five are considered in the sum, 
+#   e.g. 3, 5, 6, 9, 10, 12, 15 for n=17
+
+read -p "fizzbuzz triangle?: " fbtri
+
+n=$(($fbtri))
+sum=0
+while (( n > 0 )); do 
+    #echo "n $n"
+    if (( $(($n % 3)) == 0 )); then
+        printf "fizz"
+        let sum=sum+n
+    fi;
+    if (( $n % 5 == 0 )); then
+        let sum=sum+n
+        printf "buzz"
+    fi;
+    let n=n-1
+    printf '%s\n' "$(($n+1))"
+done
+
+echo "fizzbuzz triangle of $fbtri is $sum"
+
+
+exit;
