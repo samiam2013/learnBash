eb0a2b286f4165b5f733964d8cbe273bf4683a18
Samuel Myres <samiam2013@gmail.com>
2021-06-11 00:10:41 -0500

    leap years are weird.

--- ex_10_print_20_ly.sh
+++ ex_10_print_20_ly.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 # Write a program that prints the next 20 leap years.
 
+# what's the leap year rule?
+# says google : year % 4 == 0 || (year % 100 && year % 400)
+
+year=`date +"%Y"`
+yearN=1
+while (( $yearN < 21 )); do
+    let year=year+1
+    if (( $year % 4 == 0 )); then
+        if (( $year % 100 == 0 )); then 
+            if (( $year % 400 == 0 )); then 
+                echo "$yearN: $year" 
+                let yearN=yearN+1
+            else 
+                continue;
+            fi
+        else
+            echo "$yearN: $year"
+            let yearN=yearN+1
+        fi
+    fi
+done
 
 
 exit;
