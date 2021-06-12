0ab8fd8995274e133f2e64423adfd0c077390b4f
Samuel Myres <samiam2013@gmail.com>
2021-06-12 10:35:37 -0500

    returning from bash functions is funny, so if it's a single script can I just print it?

--- ListsAndStrings/ex_01_ret_largest_elem.sh
+++ ListsAndStrings/ex_01_ret_largest_elem.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
+# 1 Write a function that returns the largest element in a list.
 
-# I already forgot how to make arrays in bash.
+num_array=(-20 5 73 -2)
+
+max_element () {
+    nums=("$@")
+    min=0
+    max=0
+    for num in "${nums[@]}"; do
+        if (($num < $min)); then
+            let min=$num
+            if (($max == 0)); then
+                let max=$min
+            fi
+        fi
+        if (($num > $max)); then
+            let max=$num
+        fi
+    done
+    echo $max
+}
+
+max_element "${num_array[@]}"
+
+
+exit;
